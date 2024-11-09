3b150718c39b8a6f3de66be9ef991b5ae30c5132
Pothuguntla Pranitha <52751999+Pothugunt1a@users.noreply.github.com>
2024-11-08 19:33:12 -0600

    Update resolvers.js

--- app/resolvers.js
+++ app/resolvers.js
@@ -5,7 +5,6 @@ const Art = require('../models/Art');
 const UserRole = require('../models/UserRole');
 const Payment = require('../models/Payment');
 // Import other models as needed
-
 const resolvers = {
   Query: {
     users: async () => await User.find(),
@@ -132,6 +131,7 @@ const resolvers = {
           event_venue: eventDetails?.eventVenue,
           event_time: eventDetails?.eventTime
         });
+
         await payment.save();
         console.log('Payment saved to MongoDB:', payment);
 
