3e72da23b50b7fe2c4e87e35d3aa0b562970c38d
Pothuguntla Pranitha <52751999+Pothugunt1a@users.noreply.github.com>
2024-12-03 21:57:35 -0600

    Update Artist.js
    Artist login and signup

--- models/Artist.js
+++ models/Artist.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const ArtistSchema = new mongoose.Schema({
     artist_id: {
@@ -6,15 +8,56 @@ const ArtistSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    name: {
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
         type: String,
         required: true,
     },
     bio: {
         type: String,
     },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
 }, {
-    timestamps: false
+    timestamps: true
 });
+
+// Hash password before saving
+ArtistSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+// Generate JWT token
+ArtistSchema.methods.generateAuthToken = function() {
+    return jwt.sign(
+        { id: this.artist_id, email: this.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+    );
+};
+
+// Compare password
+ArtistSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('Artist', ArtistSchema);
