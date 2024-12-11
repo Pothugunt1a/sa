c5d52f321f03ab19f56209e2e05b1e867fc44ec8
Pothuguntla Pranitha <52751999+Pothugunt1a@users.noreply.github.com>
2024-12-10 22:58:13 -0600

    Update Artist.js
    updated for artist login and registration

--- models/Artist.js
+++ models/Artist.js
@@ -1,78 +1,88 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const crypto = require('crypto');
 
 const ArtistSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
+        required: [true, 'First name is required']
     },
     lastName: {
         type: String,
-        required: true,
+        required: [true, 'Last name is required']
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required'],
         unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        minlength: 6
     },
-    phone: {
+    phoneNumber: {
         type: String,
-        required: true,
+        required: [true, 'Phone number is required']
     },
-    city: String,
-    state: String,
-    country: String,
+    city: {
+        type: String,
+        required: [true, 'City is required']
+    },
+    state: {
+        type: String,
+        required: [true, 'State is required']
+    },
+    country: {
+        type: String,
+        required: [true, 'Country is required']
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     bio: String,
     profileImage: String,
-    displayName: String,
-    address: String,
-    subscription: {
-        type: String,
-        default: 'free'
-    },
-    publicLink: String,
-    socialLinks: {
-        facebook: String,
-        instagram: String
-    },
     isVerified: {
         type: Boolean,
         default: false
     },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date
+    verificationToken: String
 }, {
     timestamps: true
 });
 
-ArtistSchema.plugin(AutoIncrement, { inc_field: 'artist_id' });
-
 // Hash password before saving
 ArtistSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 12);
-    }
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
     next();
 });
 
 // Generate JWT token
 ArtistSchema.methods.generateAuthToken = function() {
     return jwt.sign(
-        { id: this.artist_id, email: this.email },
+        { id: this._id, email: this.email },
         process.env.JWT_SECRET,
         { expiresIn: '24h' }
     );
 };
 
 // Compare password
-ArtistSchema.methods.comparePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
+ArtistSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Generate password reset token
+ArtistSchema.methods.generatePasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+    this.resetPasswordExpires = Date.now() + 30 * 60 * 1000; // 30 minutes
+    return resetToken;
 };
 
 module.exports = mongoose.model('Artist', ArtistSchema);
